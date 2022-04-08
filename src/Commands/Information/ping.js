a19548a4e4a2de4fdbd7533c3d33bc31697ad848
yassineibr <yassineibr1@gmail.com>
2022-04-08 09:33:26 +0000

    Changed the ping command to show Latency

--- src/Commands/Information/ping.js
+++ src/Commands/Information/ping.js
@@ -10,6 +10,7 @@ module.exports = {
      */
     async execute({client, interaction }){
       
-        await interaction.reply({ content: `PONG after \`${client.ws.ping}ms\``, ephemeral: true });
+        // await interaction.reply({ content: `PONG after \`${client.ws.ping}ms\``, ephemeral: true });
+        await interaction.reply({ content: `🏓Latency is \`${Date.now() - interaction.createdTimestamp}ms\`. API Latency is \`${Math.round(client.ws.ping)}ms\`.`, ephemeral: true });
     }
 }
