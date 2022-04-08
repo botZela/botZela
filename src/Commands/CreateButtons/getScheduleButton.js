3b9108ccd8901323f4fb4946773dec1fffb4db40
yassineibr <yassineibr1@gmail.com>
2022-04-08 14:22:16 +0000

    Changed the name of schedule button to 'waa tarii9'

--- src/Commands/CreateButtons/getScheduleButton.js
+++ src/Commands/CreateButtons/getScheduleButton.js
@@ -14,16 +14,18 @@ module.exports = {
             new MessageEmbed()
                 .setColor("RED")
                 .setTitle("Get your Schedule Customised for You")
-                .setDescription("To get your Custom Schedule just type whatever you want in this channel, you will get it in Direct Messages.\nOr just press the button below ,`📅 Get Schedule`, for more conveniance.\n")
+                .setDescription("To get your Custom Schedule just type whatever you want in this channel, you will get it in Direct Messages.\nOr just press the button below ,`📅 Get Schedule`, for more convenience.\n")
                 .addField(
                     "Any Suggestions",
                     `Consider sending us your feedback in <#922875567357984768>, Thanks.`
                 )
+
         row.addComponents(
             new MessageButton()
                 .setCustomId("sendSchedule")
-                .setLabel("📅 Get Schedule")
-                .setStyle("SECONDARY")
+                .setLabel("Waa Tarii9 Siift l'emploi")
+                .setStyle("SUCCESS")
+                .setEmoji("📅")
         )
 
         // TODO : Hide the /button_schedule
