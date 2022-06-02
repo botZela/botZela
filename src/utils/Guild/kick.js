0f4e5dd5e4059ed323a9f23571945377829a20ae
yassineibr <75940849+yassineibr@users.noreply.github.com>
2022-06-02 17:53:24 +0100

    fixed kick function

--- src/utils/Guild/kick.js
+++ src/utils/Guild/kick.js
@@ -1,21 +1,22 @@
 async function kick(member, guild) {
-    if ( !(["921408078983876678","979396566018322482"].includes(toString(guild.id)))){
+    if ( !(["921408078983876678", "979396566018322482"].includes(guild.id))){
+        console.log("Halloo");
         return;
     } 
-    let formUrl,inviteUrl;
+    let formURL,inviteURL;
     if (guild.id == "979396566018322482"){
-        formUrl = "https://forms.gle/oyKyzc225jw7vAhc8"; 
-        inviteUrl = "https://discord.gg/27khZuVstQ"; 
+        formURL = "https://forms.gle/oyKyzc225jw7vAhc8"; 
+        inviteURL = "https://discord.gg/27khZuVstQ"; 
     } else if (guild.id == "921408078983876678"){
-        formUrl = "https://forms.gle/6XoXUAZiRkvWw8Rp9" ; 
-        inviteUrl = "https://discord.gg/yDbrgnnC4D" ; 
+        formURL = "https://forms.gle/6XoXUAZiRkvWw8Rp9" ; 
+        inviteURL = "https://discord.gg/yDbrgnnC4D" ; 
     }
 
     await member.send(`You did not fill the form correctly(like we said it is automated and you got kicked from the server).\n\
-Please Consider refilling the form using this username : \`\`\`${member.name}\`\`\` in the Discord Username:\n\n\
+Please Consider refilling the form using this username : \`\`\`${member.user.tag}\`\`\` in the Discord Username:\n\n\
 ${formURL}\n\n\
 After refiling the form you can rejoin the server without getting kicked\n\n\
-${inviteUrl}\n\
+${inviteURL}\n\n\
 Thanks for your Understanding.`);
     await member.kick();
 }
