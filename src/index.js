f9042cd4013b91da2c5def5ee195fce2ddd6318c
yassineibr <75940849+yassineibr@users.noreply.github.com>
2022-06-23 22:29:35 +0100

    Changed the way the testServers are setted

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const { Client, Intents, Collection } = require("discord.js");
-const { TOKEN } = require("../credentials/config.json");
+const { TOKEN, testGuilds } = require("../credentials/config.json");
 
 const { promisify } = require("util");
 const { glob } = require("glob");
@@ -16,21 +16,18 @@ client.selectMenu = new Collection();
 
 client.data = require("../data/data.json");
 
-client.testGuilds = [
+client.testGuilds = testGuilds || [
     { name: "🔺▬▬ • ENSIAS • ▬▬🔺", id: "921408078983876678" },
     { name: "Test_channel", id: "942172171285987370" },
     { name: "bot TEST WN", id: "933499256000643103" },
     { name: "GIVE'N'TAKE", id: "979396566018322482" },
     { name: "ENACTUS ENSIAS", id: "987024965431881768" },
     { name: "INSEC ENSIAS", id: "988184093797789746" },
-    // { name: "IEEE", id: "981579286735634492" },
 ];
 
 ["Events", "Commands", "Buttons", "SelectMenu"].forEach((handler) => {
     require(`./Handlers/${handler}`)(client, PG, AsciiTable3);
 });
-
-
 
 client.login(TOKEN);
 
