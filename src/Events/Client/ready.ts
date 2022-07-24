e4d0ec1294d41be9e0dc5c3b868f8f2be3e57371
yassineibr <75940849+yassineibr@users.noreply.github.com>
2022-07-24 13:00:57 +0100

    feat presence counter for ensias server

--- src/Events/Client/ready.ts
+++ src/Events/Client/ready.ts
@@ -1,7 +1,34 @@
-import { ActivityType } from 'discord.js';
+import { ActivityType, ChannelType } from 'discord.js';
 import mongoose, { ConnectOptions } from 'mongoose';
 import { client } from '../..';
 import { Event } from '../../Structures';
+
+function presenceUpdate() {
+	client.channels
+		.fetch('1000733341529673728')
+		.then((presChannel) => {
+			if (!presChannel || presChannel.type !== ChannelType.GuildVoice) return;
+			const guild = presChannel.guild;
+			const totalMembers = guild.members.cache.size;
+			const onlineMembers = guild.members.cache.filter((m) => m.presence?.status === 'online').size;
+			const idleMembers = guild.members.cache.filter((m) => m.presence?.status === 'idle').size;
+			const dndMembers = guild.members.cache.filter((m) => m.presence?.status === 'dnd').size;
+			const offlineMembers = totalMembers - onlineMembers - idleMembers - dndMembers;
+			const online = `🟢 ${onlineMembers}`;
+			const idle = `🌙 ${idleMembers}`;
+			const dnd = `⛔ ${dndMembers}`;
+			const offline = `⚫ ${offlineMembers}`;
+			presChannel
+				.edit({ name: `${online} ${idle} ${dnd} ${offline}` })
+				.then(() => {
+					setTimeout(presenceUpdate, 6 * 60 * 1000);
+				})
+				.catch(console.error);
+		})
+		.catch((e) => {
+			throw e;
+		});
+}
 
 const defaultExport: Event<'ready'> = {
 	name: 'ready',
@@ -17,6 +44,9 @@ const defaultExport: Event<'ready'> = {
 			status: 'online',
 		});
 		console.log(`[INFO] Ready! Logged in as ${user?.tag ?? 'BOT'}`);
+
+		// Presence ( online dnd offline ) for ensias guild
+		presenceUpdate();
 
 		// The connection to the Database(MongoDB)
 		if (!process.env.DatabaseUri) return console.log('[INFO] -----------------');
