72074eebd3d6a9daf09bcc4b3e751de05370031b
yassineibr <75940849+yassineibr@users.noreply.github.com>
2022-07-24 12:08:19 +0100

    remove Permissions array and embedValidator files

--- src/Validation/index.ts
+++ src/Validation/index.ts
@@ -1,2 +1,1 @@
 export { Events } from './EventNames';
-export { Perms } from './PermissionNames';
--- src/utils/SendEmbed/buildEmbed.ts
+++ src/utils/SendEmbed/buildEmbed.ts
@@ -1,15 +1,9 @@
 import { APIEmbed } from 'discord.js';
-// import yaml from "js-yaml";
-// import { messageEmbedOptionsSchema } from '../../Validation/EmbedStructure';
 
 export function buildEmbed(structure: string | null): APIEmbed | null {
 	try {
 		if (structure) {
 			const embed = JSON.parse(structure) as APIEmbed;
-			// const embed = yaml.load(structure.replaceAll('\\n','\n')) as MessageEmbedOptions;
-			// const embedOptions = messageEmbedOptionsSchema.parse(embed);
-			// console.log(embedOptions);
-			// return embedOptions;
 			return embed;
 		}
 		return null;
