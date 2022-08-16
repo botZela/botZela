75fc9938bd2bf92c7c3b1de636b9944f4648d24a
yassineibr <75940849+yassineibr@users.noreply.github.com>
2022-08-16 12:40:38 +0100

    fix fetching filiere

--- src/utils/Schedule/flGrp.ts
+++ src/utils/Schedule/flGrp.ts
@@ -4,8 +4,11 @@ import { FiliereType, GroupeType, YearType } from '../../Typings/Ensias';
 export function flGrpYr(member: GuildMember) {
 	const roles = member.roles.cache.map((r) => ({ name: r.name, id: r.id }));
 
-	const filieresArray = ['2IA', '2SCL', 'BI&A', 'GD', 'GL', 'IDF', 'IDSIT', 'SSE', 'SSI'];
-	const fl = roles.filter(({ name }) => filieresArray.includes(name.slice(1, -1))).at(0) as FiliereType;
+	const filieresArray = ['_2IA_', '_2SCL_', '_BI&A_', '_GD_', '_GL_', '_IDF_', '_IDSIT_', '_SSE_', '_SSI_'];
+	const fl = roles
+		.filter(({ name }) => filieresArray.includes(name))
+		.map(({ id, name }) => ({ id, name: name.slice(1, -1) }))
+		.at(0) as FiliereType;
 
 	const groupesArray = ['G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8'];
 	const grp = roles.filter(({ name }) => groupesArray.includes(name)).at(0) as GroupeType;
