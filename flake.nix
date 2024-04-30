1d235c5089eab11f48452fe644a800ad743a0606
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-04-30 22:40:24 +0100

    update flake.{nix,lock}

--- flake.nix
+++ flake.nix
@@ -1,37 +1,34 @@
 {
   # Override nixpkgs to use the latest set of node packages
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    ,
-    }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
     flake-utils.lib.eachDefaultSystem
-      (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            nodejs
-            # You can set the major version of Node.js to a specific one instead
-            # of the default version
-            # nodejs-19_x
-
-            # You can choose pnpm, yarn, or none (npm).
-            nodePackages.pnpm
-            # yarn
-            flyctl
-          ];
-
-          shellHook = ''
-            [ -d node_modules ] || pnpm i
-          '';
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
       };
-      });
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs
+          # You can set the major version of Node.js to a specific one instead
+          # of the default version
+          # nodejs-19_x
+
+          # You can choose pnpm, yarn, or none (npm).
+          nodePackages.pnpm
+          # yarn
+          flyctl
+        ];
+
+        shellHook = ''
+          [ -d node_modules ] || pnpm i
+        '';
+      };
+    });
 }
